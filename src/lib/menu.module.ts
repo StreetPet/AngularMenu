5ca7a875fac537da38815ba31e3b1d4613d852a0
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-02-03 21:22:29 -0300

    TEstando uma nova abordagem para a barra de navegação.
    Precisamos ter uma barra de navegação unificada, que irá validar a
    disponbilidade dos links conforme quem está logado, habilitando com for
    o caso os botões para o perfil do voluntário logado.
    
    A mesma barra será usada em toda as aplicações, devendo portanto
    aproveitar a seçaõ de login entre apps.

--- src/lib/menu.module.ts
+++ src/lib/menu.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MenuComponent } from './menu.component';
 import { CommonModule, NgIf } from '@angular/common';
-import { MatIcon, MatIconModule, MatMenuModule } from '@angular/material';
+import { MatIcon, MatIconModule, MatMenuModule, MatGridListModule, MatGridTile } from '@angular/material';
 
 @NgModule({
   declarations: [MenuComponent],
   imports: [
     MatMenuModule,
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule
   ],
   exports: [MenuComponent]
 })
